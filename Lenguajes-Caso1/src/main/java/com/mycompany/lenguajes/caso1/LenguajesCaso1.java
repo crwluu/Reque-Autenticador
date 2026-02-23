18743553faecd324b608eb55b677b243ae9652f9
Lun <165858477+crwluu@users.noreply.github.com>
2026-02-23 12:20:17 -0600

    Add files via upload

---
+++ Lenguajes-Caso1/src/main/java/com/mycompany/lenguajes/caso1/LenguajesCaso1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lenguajes.caso1;
+
+/**
+ *
+ * @author Luan Valverde
+ */
+public class LenguajesCaso1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
