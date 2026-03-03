e370c00612b46134bb070853099c2ab6fdd1fa33
Lun <165858477+crwluu@users.noreply.github.com>
2026-03-02 21:57:36 -0600

    Update register.js

--- Logic/register.js
+++ Logic/register.js
@@ -1,29 +1,27 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-
     // buscar boton
     const registerButton = document.getElementById("registerButton");
     if (!registerButton) return;
 
     // listener del boton
     registerButton.addEventListener("click", () => {
-
         // input
         const name = document.getElementById("fullname").value;
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
 
         try {
-            // validaciones
+            // validacioes
             if (!name) throw new FieldIsEmptyException("Full name");
             if (!email) throw new FieldIsEmptyException("Email");
             if (!password) throw new FieldIsEmptyException("Password");
 
+            // intentar crear cuenta
             controller.addAccount(name, email, password);
 
             alert("Account created. You can now log in.");
 
-            // redirigir
             window.location.href = "index.html";
 
         } catch (error) {alert(error.message);}
